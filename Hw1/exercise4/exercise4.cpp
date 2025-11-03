5eae5eee81ebc274d5fc8431a23809cb1f7b808a
PANAGIVTHS <panagiotis.tsogka@gmail.com>
2025-11-04 01:53:41 +0200

    Refactor, Add filled circles

--- Hw1/exercise4/exercise4.cpp
+++ Hw1/exercise4/exercise4.cpp
@@ -9,8 +9,12 @@
 #define MAX_LINES 1000
 
 typedef struct {
-    int x, y;
     float red, green, blue;
+} RGB;
+
+typedef struct {
+    int x, y;
+    RGB rgb;
 } Point;
 
 typedef struct {
@@ -19,11 +23,20 @@ typedef struct {
 } Line;
 
 Line lines[MAX_LINES];
-Point curColor;
+RGB curColor;
 Line tempLine;
+Line *newestLine;
 
 int lineCount = 0, isSecClick = -1, reshapeState = 0;
 int newLineIdx = 0;
+
+int max(int num1, int num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+int sign(int number) {
+    return (number > 0) - (number < 0);
+}
 
 void windowToWorldCoord(int *x, int *y) {
     float halfWidth  = glutGet(GLUT_WINDOW_WIDTH) / 2.0f;
@@ -33,14 +46,6 @@ void windowToWorldCoord(int *x, int *y) {
     *y = halfHeight - *y;
 }
 
-int sign(int number) {
-    return (number > 0) - (number < 0);
-}
-
-int max(int num1, int num2) {
-    return num1 > num2 ? num1 : num2;
-}
-
 void drawPoint(int x, int y) {
     glBegin(GL_POINTS);
     glVertex2i(x, y);
@@ -48,36 +53,38 @@ void drawPoint(int x, int y) {
     glFlush();
 }
 
+void storeLine(Line line) {
+    lines[newLineIdx] = line;
+    if (lineCount != MAX_LINES) lineCount++;
+    newLineIdx = (newLineIdx + 1) % MAX_LINES;
+    int curLine = newLineIdx == 0 ? lineCount - 1 : newLineIdx - 1;
+    newestLine = &lines[curLine];
+}
+
 void mouseHandler(int button, int state, int x, int y) {
-    if (state != GLUT_DOWN) return; // Ignore mouse release
+    if (state != GLUT_DOWN) return; //! Ignore mouse release
 
     int localX = x, localY = y;
     windowToWorldCoord(&localX, &localY);
     
     if (button == GLUT_LEFT_BUTTON) {
         if (isSecClick == 1) {
-            lines[newLineIdx].start.x = tempLine.start.x;
-            lines[newLineIdx].start.y = tempLine.start.y;
-            lines[newLineIdx].start.red = tempLine.start.red;
-            lines[newLineIdx].start.green = tempLine.start.green;
-            lines[newLineIdx].start.blue = tempLine.start.blue;
-            lines[newLineIdx].end.x = localX;
-            lines[newLineIdx].end.y = localY;
-            lines[newLineIdx].end.red = curColor.red;
-            lines[newLineIdx].end.green = curColor.green;
-            lines[newLineIdx].end.blue = curColor.blue;
+            tempLine.end.x = localX;
+            tempLine.end.y = localY;
+            tempLine.end.rgb.red = curColor.red;
+            tempLine.end.rgb.green = curColor.green;
+            tempLine.end.rgb.blue = curColor.blue;
             isSecClick = 0;
-            if (lineCount != MAX_LINES) lineCount++;
-            newLineIdx = (newLineIdx + 1) % MAX_LINES;
+            storeLine(tempLine);
             glutPostRedisplay();
         } else {
             //! Incase someone doesnt acutally go through with creating the line
             //! Dont overwrite previous' lines data when on max capacity
             tempLine.start.x = localX;
             tempLine.start.y = localY;
-            tempLine.start.red = curColor.red;
-            tempLine.start.green = curColor.green;
-            tempLine.start.blue = curColor.blue;
+            tempLine.start.rgb.red = curColor.red;
+            tempLine.start.rgb.green = curColor.green;
+            tempLine.start.rgb.blue = curColor.blue;
             isSecClick = 1;
         }
     } else {
@@ -85,6 +92,104 @@ void mouseHandler(int button, int state, int x, int y) {
         newLineIdx = 0;
         glClear(GL_COLOR_BUFFER_BIT);
         glFlush();
+    }
+}
+
+void drawLine(Line line) {
+    RGB delta;
+    int condSlope = 0, error = 0;
+    int leadingAxis = 0, trailingAxis = 0, endPointCoord = 0;
+    int signLeadingAxis = 0, signTrailingAxis = 0,
+        dstLeadingAxis = 0, dstTrailingAxis = 0;
+    
+    Point endPoint = line.end;
+    Point startPoint = line.start;
+
+    // Calculate axis distance 
+    int dstX = abs(endPoint.x - startPoint.x);
+    int dstY = abs(endPoint.y - startPoint.y);
+    
+    // Set default color
+    glColor3f(startPoint.rgb.red, startPoint.rgb.green, startPoint.rgb.blue);
+    
+    // Starting point == End point
+    if (!(dstX || dstY)) {
+        drawPoint(startPoint.x, startPoint.y);
+        return;
+    }
+    
+    int numPixels = max(dstX, dstY);
+    condSlope = dstX >= dstY;
+
+    delta.red = (endPoint.rgb.red - startPoint.rgb.red) / numPixels;
+    delta.green = (endPoint.rgb.green - startPoint.rgb.green) / numPixels;
+    delta.blue = (endPoint.rgb.blue - startPoint.rgb.blue) / numPixels;
+
+    glBegin(GL_POINTS); 
+
+    // Pick leading and trailing axis
+    if (condSlope) {
+        // Initialise points
+        leadingAxis = startPoint.x;
+        trailingAxis = startPoint.y;
+        endPointCoord = endPoint.x;
+        
+        // Initialise axis variables
+        signLeadingAxis = sign(endPoint.x - startPoint.x);
+        signTrailingAxis = sign(endPoint.y - startPoint.y);
+        dstLeadingAxis = dstX;
+        dstTrailingAxis = dstY;
+        
+        // Draw starting point
+        glVertex2i(leadingAxis, trailingAxis);
+    } else {
+        // Initialise points
+        leadingAxis = startPoint.y;
+        trailingAxis = startPoint.x;
+        endPointCoord = endPoint.y;
+        // Initialise axis variables
+        signLeadingAxis = sign(endPoint.y - startPoint.y);
+        signTrailingAxis = sign(endPoint.x - startPoint.x);
+        dstLeadingAxis = dstY;
+        dstTrailingAxis = dstX;
+        
+        // Draw starting point
+        glVertex2i(trailingAxis, leadingAxis);
+    }
+
+    // Initialise error variable
+    error = (2 * dstTrailingAxis) - dstLeadingAxis;
+    
+    //? Update draw buffer every line instead of pixel
+    //? This is faster and per pixel makes 0 sense
+    while (leadingAxis != endPointCoord) {
+        error += 2 * dstTrailingAxis;
+        leadingAxis += signLeadingAxis;
+    
+        if (error >= 0) {
+            error -= 2 * dstLeadingAxis;
+            trailingAxis += signTrailingAxis;
+        }
+        
+        // Draw pixel at specified point and increment deltas
+        startPoint.rgb.red += delta.red;
+        startPoint.rgb.green += delta.green;
+        startPoint.rgb.blue += delta.blue;
+        glColor3f(startPoint.rgb.red, startPoint.rgb.green, startPoint.rgb.blue);
+        if (condSlope) {
+            glVertex2i(leadingAxis, trailingAxis);
+        } else {
+            glVertex2i(trailingAxis, leadingAxis);
+        }
+    }
+    glEnd();
+}
+
+void redrawAll() {
+    for (int i = 0; i < lineCount; i++) {
+        //! if you want to keep precedence start from 
+        //! newLineIdx and move circularly instead of i
+        drawLine(lines[i]);
     }
 }
 
@@ -103,108 +208,16 @@ void init() {
     gluOrtho2D(-300, 300, -300, 300); 
 }
 
-void drawLine(Point startPoint, Point endPoint) {
-    Point delta; //! Dummy point to store delta values
-    int condSlope = 0, error = 0;
-    int leadingAxis = 0, trailingAxis = 0, endPointCoord = 0;
-    int signLeadingAxis = 0, signTrailingAxis = 0,
-        dstLeadingAxis = 0, dstTrailingAxis = 0;
-
-    // Calculate axis distance 
-    int dstX = abs(endPoint.x - startPoint.x);
-    int dstY = abs(endPoint.y - startPoint.y);
-    
-    // Set default color
-    glColor3f(startPoint.red, startPoint.green, startPoint.blue);
-    
-    // Starting point == End point
-    if (!(dstX || dstY)) {
-        drawPoint(startPoint.x, startPoint.y);
-        return;
-    }
-    
-    int numPixels = max(dstX, dstY);
-    condSlope = dstX >= dstY;
-
-    //! This can be u_int. Could be faster and more efficient 
-    delta.red = (endPoint.red - startPoint.red) / numPixels;
-    delta.green = (endPoint.green - startPoint.green) / numPixels;
-    delta.blue = (endPoint.blue - startPoint.blue) / numPixels;
-
-    // Pick leading and trailing axis
-    if (condSlope) {
-        // Initialise points
-        leadingAxis = startPoint.x;
-        trailingAxis = startPoint.y;
-        endPointCoord = endPoint.x;
-        
-        // Initialise axis variables
-        signLeadingAxis = sign(endPoint.x - startPoint.x);
-        signTrailingAxis = sign(endPoint.y - startPoint.y);
-        dstLeadingAxis = dstX;
-        dstTrailingAxis = dstY;
-        
-        // Draw starting point
-        drawPoint(leadingAxis, trailingAxis);
-    } else {
-        // Initialise points
-        leadingAxis = startPoint.y;
-        trailingAxis = startPoint.x;
-        endPointCoord = endPoint.y;
-        
-        // Initialise axis variables
-        signLeadingAxis = sign(endPoint.y - startPoint.y);
-        signTrailingAxis = sign(endPoint.x - startPoint.x);
-        dstLeadingAxis = dstY;
-        dstTrailingAxis = dstX;
-        
-        // Draw starting point
-        drawPoint(trailingAxis, leadingAxis);
-    }
-
-    // Initialise error variable
-    error = (2 * dstTrailingAxis) - dstLeadingAxis;
-    
-    //? Update draw buffer every line instead of pixel
-    //? This is faster and per pixel makes 0 sense
-    glBegin(GL_POINTS); 
-    while (leadingAxis != endPointCoord) {
-        error += 2 * dstTrailingAxis;
-        leadingAxis += signLeadingAxis;
-    
-        if (error >= 0) {
-            error -= 2 * dstLeadingAxis;
-            trailingAxis += signTrailingAxis;
-        }
-        
-        // Draw pixel at specified point and increment deltas
-        startPoint.red += delta.red;
-        startPoint.green += delta.green;
-        startPoint.blue += delta.blue;
-        glColor3f(startPoint.red, startPoint.green, startPoint.blue);
-        if (condSlope) {
-            glVertex2i(leadingAxis, trailingAxis);
-        } else {
-            glVertex2i(trailingAxis, leadingAxis);
-        }
-    }
-    glEnd();
-}
-
 void display(void) {
     //! Avoid initial call when window is created
     if (isSecClick == -1) return;
     
     if (reshapeState == 1) {
-        for (int i = 0; i < lineCount; i++) {
-            //! if you want to keep precedence start from 
-            //! newLineIdx and move circularly instead of i
-            drawLine(lines[i].start, lines[i].end);
-        }
+        //! Redraw all object relative to new window
+        redrawAll();
         reshapeState = 0;
     } else {
-        int curLine = newLineIdx == 0 ? lineCount - 1 : newLineIdx - 1;
-        drawLine(lines[curLine].start, lines[curLine].end);
+        drawLine(*newestLine);
     }
     glFlush();
 }
@@ -246,13 +259,10 @@ void reshapeHandler(int w, int h) {
     gluOrtho2D(-w / 2, w / 2, -h / 2, h / 2);
     glClear(GL_COLOR_BUFFER_BIT);
     glFlush();
-    
     reshapeState = 1; //! Notify display to redraw ALL lines
     glutPostRedisplay();
 }
 
-// TODO Figure out if the color is meant to reset to red every new line
-// TODO or just the first time the program starts??
 int main(int argc, char** argv) {
 
     glutInit(&argc, argv);
--- Hw1/exercise5/exercise5.cpp
+++ Hw1/exercise5/exercise5.cpp
@@ -7,25 +7,148 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define MAX_LINES 1000
+
+#define MAKE_OFST_POINT(base, dx, dy, col) ((Point){ (base).x + (dx), (base).y + (dy), (col) })
+
 typedef struct {
-    int xCord, yCord;
+    float red, green, blue;
+} RGB;
+
+typedef struct {
+    int x, y;
+    RGB rgb;
 } Point;
 
 typedef struct {
-    int xRadius, yRadius;
-    int radius;
-} PointRadius;
+    Point start;
+    Point end;
+} Line;
 
-Point firstCoord;
-PointRadius secondCoord; 
+Line lines[MAX_LINES];
+RGB curColor;
+Line tempLine;
+Line *newestLine;
+Point startPoint, endPoint; 
 
-int isSecClick = 0;
+int lineCount = 0;
+int newLineIdx = 0;
+int isSecClick = -1;
 
-int toThePower2(int number) {
-    return number * number;
+int max(int num1, int num2) {
+    return num1 > num2 ? num1 : num2;
 }
 
-void WindowToWorldCoord(int *x, int *y) {
+int sign(int number) {
+    return (number > 0) - (number < 0);
+}
+
+void drawPoint(int x, int y) {
+    glBegin(GL_POINTS);
+    glVertex2i(x, y);
+    glEnd();
+    glFlush();
+}
+
+void storeLine(Line line) {
+    lines[newLineIdx] = line;
+    if (lineCount != MAX_LINES) lineCount++;
+    newLineIdx = (newLineIdx + 1) % MAX_LINES;
+    int curLine = newLineIdx == 0 ? lineCount - 1 : newLineIdx - 1;
+    newestLine = &lines[curLine];
+}
+
+void drawLine(Line line) {
+    RGB delta;
+    int condSlope = 0, error = 0;
+    int leadingAxis = 0, trailingAxis = 0, endPointCoord = 0;
+    int signLeadingAxis = 0, signTrailingAxis = 0,
+        dstLeadingAxis = 0, dstTrailingAxis = 0;
+    
+    Point endPoint = line.end;
+    Point startPoint = line.start;
+
+    // Calculate axis distance 
+    int dstX = abs(endPoint.x - startPoint.x);
+    int dstY = abs(endPoint.y - startPoint.y);
+    
+    // Set default color
+    glColor3f(startPoint.rgb.red, startPoint.rgb.green, startPoint.rgb.blue);
+    
+    // Starting point == End point
+    if (!(dstX || dstY)) {
+        drawPoint(startPoint.x, startPoint.y);
+        return;
+    }
+    
+    int numPixels = max(dstX, dstY);
+    condSlope = dstX >= dstY;
+
+    delta.red = (endPoint.rgb.red - startPoint.rgb.red) / numPixels;
+    delta.green = (endPoint.rgb.green - startPoint.rgb.green) / numPixels;
+    delta.blue = (endPoint.rgb.blue - startPoint.rgb.blue) / numPixels;
+
+    glBegin(GL_POINTS); 
+
+    // Pick leading and trailing axis
+    if (condSlope) {
+        // Initialise points
+        leadingAxis = startPoint.x;
+        trailingAxis = startPoint.y;
+        endPointCoord = endPoint.x;
+        
+        // Initialise axis variables
+        signLeadingAxis = sign(endPoint.x - startPoint.x);
+        signTrailingAxis = sign(endPoint.y - startPoint.y);
+        dstLeadingAxis = dstX;
+        dstTrailingAxis = dstY;
+        
+        // Draw starting point
+        glVertex2i(leadingAxis, trailingAxis);
+    } else {
+        // Initialise points
+        leadingAxis = startPoint.y;
+        trailingAxis = startPoint.x;
+        endPointCoord = endPoint.y;
+        // Initialise axis variables
+        signLeadingAxis = sign(endPoint.y - startPoint.y);
+        signTrailingAxis = sign(endPoint.x - startPoint.x);
+        dstLeadingAxis = dstY;
+        dstTrailingAxis = dstX;
+        
+        // Draw starting point
+        glVertex2i(trailingAxis, leadingAxis);
+    }
+
+    // Initialise error variable
+    error = (2 * dstTrailingAxis) - dstLeadingAxis;
+    
+    //? Update draw buffer every line instead of pixel
+    //? This is faster and per pixel makes 0 sense
+    while (leadingAxis != endPointCoord) {
+        error += 2 * dstTrailingAxis;
+        leadingAxis += signLeadingAxis;
+    
+        if (error >= 0) {
+            error -= 2 * dstLeadingAxis;
+            trailingAxis += signTrailingAxis;
+        }
+        
+        // Draw pixel at specified point and increment deltas
+        startPoint.rgb.red += delta.red;
+        startPoint.rgb.green += delta.green;
+        startPoint.rgb.blue += delta.blue;
+        glColor3f(startPoint.rgb.red, startPoint.rgb.green, startPoint.rgb.blue);
+        if (condSlope) {
+            glVertex2i(leadingAxis, trailingAxis);
+        } else {
+            glVertex2i(trailingAxis, leadingAxis);
+        }
+    }
+    glEnd();
+}
+
+void windowToWorldCoord(int *x, int *y) {
     float halfWidth = glutGet(GLUT_WINDOW_WIDTH) / 2.0f;
     float halfHeight = glutGet(GLUT_WINDOW_HEIGHT) / 2.0f;
 
@@ -34,72 +157,120 @@ void WindowToWorldCoord(int *x, int *y) {
 }
 
 void mouseHandler(int button, int state, int x, int y) {
-    if (state != GLUT_DOWN) return; // Ignore mouse release
+    if (state != GLUT_DOWN) return; //! Ignore mouse release
 
     int localX = x, localY = y;
-    WindowToWorldCoord(&localX, &localY);
+    windowToWorldCoord(&localX, &localY);
 
     if (button == GLUT_LEFT_BUTTON) {
         if (isSecClick == 1) {
-            secondCoord.xRadius = localX;
-            secondCoord.yRadius = localY;
-            secondCoord.radius = 
-            sqrt((toThePower2(secondCoord.xRadius - firstCoord.xCord))
-            +(toThePower2(secondCoord.yRadius - firstCoord.yCord)));  
+            endPoint.x = localX;
+            endPoint.y = localY;
             isSecClick = 0;
             glutPostRedisplay();
-        } 
-        else {
-            firstCoord.xCord = localX;
-            firstCoord.yCord = localY;
+        } else {
+            startPoint.x = localX;
+            startPoint.y = localY;
+            startPoint.rgb.red = curColor.red;
+            startPoint.rgb.green = curColor.green;
+            startPoint.rgb.blue = curColor.blue;
             isSecClick = 1;
         }
-    }
-    else {
+    } else {
         glClear(GL_COLOR_BUFFER_BIT);
         glFlush();
     }
 }
-void drawCircle(int x, int y, int radius) {
 
-    int x_center = 0, y_center = radius;
-    int x_point = x, y_point = y;
-
+void drawCircle(Point centerPoint, int radius) {
+    int xOffset = 0, yOffset = radius;
     int error = 3 - (2 * radius);
 
     glBegin(GL_POINTS);
-    while(x_center <= y_center) {
-        glVertex2i(x_point + x_center, y_point + y_center); // (x,y) point
-        glVertex2i(x_point + y_center, y_point + x_center); // (y,x) point
+    glColor3f(curColor.red, curColor.green, curColor.blue);
+    while(xOffset <= yOffset) {
+        int xPos = centerPoint.x + xOffset;
+        int xNeg = centerPoint.x - xOffset;
+        int yPos = centerPoint.y + yOffset;
+        int yNeg = centerPoint.y - yOffset;
+        int yxPos = centerPoint.x + yOffset;
+        int yxNeg = centerPoint.x - yOffset;
+        int xyPos = centerPoint.y + xOffset;
+        int xyNeg = centerPoint.y - xOffset;
 
-        glVertex2i(x_point - x_center, y_point + y_center); // (-x, y) point
-        glVertex2i(x_point + y_center, y_point - x_center); // (y, -x) point
-
-        glVertex2i(x_point - x_center, y_point - y_center); // (-x, -y) point
-        glVertex2i(x_point - y_center, y_point - x_center); // (-y, -x) point
-
-        glVertex2i(x_point + x_center, y_point - y_center); // (x, -y) point
-        glVertex2i(x_point - y_center, y_point + x_center); // (-y, x) point
+        glVertex2i(xPos,  yPos);
+        glVertex2i(yxPos, xyPos);
+        glVertex2i(xNeg,  yPos);
+        glVertex2i(yxPos, xyNeg);
+        glVertex2i(xNeg,  yNeg);
+        glVertex2i(yxNeg, xyNeg);
+        glVertex2i(xPos,  yNeg);
+        glVertex2i(yxNeg, xyPos);
     
-        x_center++;
+        xOffset++;
         if (error >= 0) {
-            y_center--;
-            error -= 4 * y_center;
+            yOffset--;
+            error -= 4 * yOffset;
         }
-        error += (4 * x_center) + 2;
+        error += (4 * xOffset) + 2;
     }
     glEnd();
     glFlush();
 }
 
+void drawCircleFilled(Point centerPoint, int radius) {
+    int xOffset = 0, yOffset = radius;
+    int error = 3 - (2 * radius);
+    Point c = centerPoint;
+
+    RGB endRGB = curColor;
+    while(xOffset <= yOffset) {
+        Point topLeft      = MAKE_OFST_POINT(c, -xOffset,  yOffset, c.rgb);
+        Point topRight     = MAKE_OFST_POINT(c,  xOffset,  yOffset, endRGB);
+        Point bottomLeft   = MAKE_OFST_POINT(c, -xOffset, -yOffset, c.rgb);
+        Point bottomRight  = MAKE_OFST_POINT(c,  xOffset, -yOffset, endRGB);
+        Point leftTop      = MAKE_OFST_POINT(c, -yOffset,  xOffset, c.rgb);
+        Point rightTop     = MAKE_OFST_POINT(c,  yOffset,  xOffset, endRGB);
+        Point leftBottom   = MAKE_OFST_POINT(c, -yOffset, -xOffset, c.rgb);
+        Point rightBottom  = MAKE_OFST_POINT(c,  yOffset, -xOffset, endRGB);
+
+        Line topSpan       = { topLeft, topRight };
+        Line bottomSpan    = { bottomLeft, bottomRight };
+        Line upperDiagSpan = { leftTop, rightTop };
+        Line lowerDiagSpan = { leftBottom, rightBottom };
+
+        drawLine(topSpan);
+        drawLine(bottomSpan);
+        drawLine(upperDiagSpan);
+        drawLine(lowerDiagSpan);
+        
+        xOffset++;
+        if (error >= 0) {
+            yOffset--;
+            error -= 4 * yOffset;
+        }
+        error += (4 * xOffset) + 2;
+    }
+}
+
 void display() {
-    drawCircle(firstCoord.xCord, firstCoord.yCord, secondCoord.radius);
+    if (isSecClick == -1) return;
+    int dstX, dstY;
+    
+    dstX = endPoint.x - startPoint.x;
+    dstY = endPoint.y - startPoint.y;
+
+    int radius = sqrt((dstX * dstX) + (dstY * dstY));  
+    drawCircleFilled(startPoint, radius);
+    glFlush();
 }
 
 void init() {
-
     // backround colour black, alpha parameter set to default
     glClearColor(0.0, 0.0, 0.0, 0.0);
+    curColor.red = 1.0f;
+    curColor.green = 0.0f;
+    curColor.blue = 0.0f;
     
     glColor3f(1.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
@@ -111,6 +282,24 @@ void init() {
 
 void keyboardHandler(unsigned char key, int x, int y) {
     switch (key) {
+        case 'r':
+        case 'R':
+            curColor.red = 1.0f;
+            curColor.green = 0.0f;
+            curColor.blue = 0.0f;
+            break;
+        case 'g':
+        case 'G':
+            curColor.red = 0.0f;
+            curColor.green = 1.0f;
+            curColor.blue = 0.0f;
+            break;
+        case 'b':
+        case 'B':
+            curColor.red = 0.0f;
+            curColor.green = 0.0f;
+            curColor.blue = 1.0f;
+            break;
         case 'Q':
         case 'q':
             exit(0);
